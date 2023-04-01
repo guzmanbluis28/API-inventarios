ef08489c0d92771a54b63a3d643fc6ca08f3472a
Luis Guzman <guzmanbluis@gmail.com>
2023-04-01 16:40:56 -0500

    Mensaje del commit

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Union
+from typing import List
 
 app = FastAPI()
-
 
 class Item(BaseModel):
     name: str
     price: float
-    is_offer: Union[bool, None] = None
+    is_offer: bool = None
 
+inventory = []
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/items/")
+async def create_item(item: Item):
+    inventory.append(item)
+    return item
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/items/", response_model=List[Item])
+async def read_items():
+    return inventory
